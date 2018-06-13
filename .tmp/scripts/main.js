7770e56b14e3d06998ecc75281ab6b4315dfd96a
Raiymbek Kurmetkhan <kraiymbek@yahoo.com>
2018-06-14 00:03:31 +0600

    added clients section

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,4 +1,16 @@
-'use strict';
+"use strict";
 
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        // dots: false,
+        nav: true,
+        navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+        navSpeed: 1000,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        smartSpeed: 1300
+    });
+});
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        // dots: false,
+        nav: true,
+        navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
+        navSpeed: 1000,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        smartSpeed: 1300
+    });
+});
