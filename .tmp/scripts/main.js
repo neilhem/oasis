a511daf85bfbee1c454a91e518c2c8458fc18950
Raiymbek Kurmetkhan <kraiymbek@yahoo.com>
2018-06-14 14:09:37 +0600

    added some animation

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -12,5 +12,13 @@ $(document).ready(function () {
         autoplayTimeout: 6000,
         smartSpeed: 1300
     });
+
+    $(".header-middle h1, .header-middle h4").addClass('animated fadeInLeft');
+    $("section .title h2, section .title p").addClass('animated fadeInRight');
+    $(".feature-left-item p, .feature-right-item p, .integration li svg").addClass('animated zoomIn');
+
+    $("section .title p").waypoint(function () {
+        console.log("asdasd");
+    });
 });
 //# sourceMappingURL=main.js.map
